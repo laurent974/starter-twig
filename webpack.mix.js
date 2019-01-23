937cb56443ce874562d7470a6a597c3fa3429da9
Laurent YEN-PON <lyenpon@webnet.fr>
2019-01-23 13:34:16 +0100

    Init

---
+++ webpack.mix.js
@@ -0,0 +1,30 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('/');
+
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+
+mix.js('app/js/app.js', 'public/bundle.js')
+  .sass('app/scss/style.scss', 'style.css')
+  .options({
+    processCssUrls: false
+  })
+  // .copyDirectory('app/assets/fonts/**/*.*', 'public/fonts')
+  .webpackConfig({
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: 'app/assets/img',
+        to: 'public/img', // Laravel mix will place this in 'public/img'
+      }]),
+      new ImageminPlugin({
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        plugins: [
+          imageminMozjpeg({
+            quality: 80,
+          })
+        ]
+      })
+    ]
+  });
